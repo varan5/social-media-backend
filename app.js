1924ed34f07feb0012ca59684ce2a9371e498a11
varan5 <varan.bhalla@gmail.com>
2022-07-20 01:56:56 +0530

    Removed static frontend files

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ const user = require("./routes/user");
 app.use("/api/v1", post);
 app.use("/api/v1", user);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 module.exports = app;
